194e02fb3fab9160759c723444be76fa49ea4aaf
w33d-w33d <chenyuqian0122@outlook.com>
2023-06-23 21:36:12 +0800

    update

---
+++ problemset/2496. 数组中字符串的最大值/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumValue(self, strs: List[str]) -> int:
+        max_val = 0
+        for s in strs:
+            # 如果字符串全为数字
+            if s.isdigit():
+                # 将字符串转化为整数并更新最大值
+                max_val = max(max_val, int(s))
+            else:
+                # 否则，以字符串的长度作为值并更新最大值
+                max_val = max(max_val, len(s))
+        return max_val
